b25829e06e00773bfdaa47f6147d77cc6a91c5f9
高若愚 <gaoruoyu25@mails.ucas.ac.cn>
2026-03-08 13:20:52 +0800

    实现内存分配接口

---
+++ ConcurrentPool/ConcurrentPool/ConcurrentPool.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "Common.hpp"
+#include "ThreadCache.hpp"
+void* AllocateMemory(size_t size)
+{
+	if (ptrThreadCache == nullptr)
+	{
+		ptrThreadCache = new ThreadCache;
+	}
+	return ptrThreadCache->Allocate(size);
+}
+void DeallocateMemory(void* mem, size_t size)
+{
+	assert(ptrThreadCache != nullptr);
+	ptrThreadCache->Deallocate(mem, size);
+}
